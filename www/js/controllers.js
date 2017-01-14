e0ad9501d7515fb6e58649fd10c816a68837ac7f
Paulo Haacke <paulohaacke@gmail.com>
2017-01-14 11:30:39 -0200

    Ionic lists advanced features part 1 exercise done.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -71,7 +71,7 @@ angular.module('ConFusion.controllers', [])
     };
 })
 
-.controller('MenuController', ['$scope', 'menuFactory', 'baseURL', function($scope, menuFactory, baseURL) {
+.controller('MenuController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', function($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
 
     $scope.baseURL = baseURL;
     $scope.tab = 1;
@@ -111,6 +111,12 @@ angular.module('ConFusion.controllers', [])
     $scope.toggleDetails = function() {
         $scope.showDetails = !$scope.showDetails;
     };
+
+    $scope.addFavorite = function(index) {
+        console.log("index is " + index);
+        favoriteFactory.addToFavorites(index);
+        $ionicListDelegate.closeOptionButtons();
+    }
 }])
 
 .controller('ContactController', ['$scope', function($scope) {
